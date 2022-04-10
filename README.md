99af11a8df6bd4416cea8b735e5e20124ed7ca66
Mina Amir <mina.amir@scandiweb.com>
2022-04-10 21:15:18 +0200

    Adding Flasher Function

--- README.md
+++ README.md
@@ -5,7 +5,14 @@
 pin: the pin number : INTEGER <br />
 timeStart: The Event Time (mm:ss): String <br />
 dur: duration of the event (sec): INTEGER <br />
+<hr/>
 
+## flasher(int pin ,char *timeStart ,int speed ,int dur)
+### Params:
+pin: the pin number : INTEGER <br />
+timeStart: The Event Time (mm:ss): String <br />
+speed: The Flashing speed (ms): INTEGER
+dur: duration of the event (sec): INTEGER <br />
 
 ## Example Code:
 
@@ -15,5 +22,9 @@ void loop() {
     // Open Motor on pin 13 @ 1:05 from the show and will end in 15 sec
    // Motor will stop @ 1:20
   event(13,"1:05":,15);
+
+   //Flash the lights on pin 13 @ 00:05 from the show with speed of 150 milliseconds
+   // for 15 seconds ie. The flashes will stop @ 00:20
+  flasher(13,"0:5",150,15);
 }
 ```
--- events_lib.h
+++ events_lib.h
@@ -17,3 +17,33 @@ void event(int pin , char *timeStart , int dur){
      digitalWrite(pin, LOW);
      }
 }
+unsigned long previousMillis = 0;
+int ledState= LOW;
+void flasher(int pin ,char *timeStart ,int speed ,int dur){
+  char time[strlen(timeStart) + 1];
+  strcpy(time, timeStart);
+  char *tok= NULL;
+  char* rest = time ;
+  tok = strtok_r(rest,":",&rest);
+  int mints = atoi(tok);
+  tok = strtok_r(rest,":",&rest);
+  int secs = atoi(tok) + (mints * 60);
+  unsigned long microStart = secs * 1000000;
+  unsigned long microStop = microStart + (dur * 1000000);
+
+if(microStart < micros() && microStop >micros())
+   {
+    if (millis() - previousMillis >= speed) {
+      previousMillis = currentMillis;
+      if (ledState == LOW) {
+      ledState = HIGH;
+      } else {
+      ledState = LOW;
+      }
+      digitalWrite(pin, ledState);
+     }
+   } else{
+       digitalWrite(pin,LOW);
+   }
+
+}
